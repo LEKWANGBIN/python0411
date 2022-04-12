6c20aacb68f12e29d9dbe9eb6c45dda981486ded
LEKWANGBIN <s25532456@naver.com>
2022-04-12 11:46:42 +0900

    함수 연습1

--- function1.py
+++ function1.py
@@ -1,8 +1,8 @@
-# function1.py
+# function1.py 
 #함수를 정의
-def dstValue(i):
+def setValue(i):
     #함수 내부의 지역변수에 초기화
-    x = i
+    x = i 
     print("내부에서 출력:", x)
 
 #함수를 호출
@@ -11,7 +11,7 @@ print(retValue)
 
 #함수를 정의
 def swap(x,y):
-    return y,x
-    
+    return y,x 
+
 #호출
 print(swap(2,3))
