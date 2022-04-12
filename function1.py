84530c5b0d9ce8f4e749c6d8d529d3518cc7a19c
LEKWANGBIN <s25532456@naver.com>
2022-04-12 13:10:49 +0900

    함수연습3

--- function1.py
+++ function1.py
@@ -15,3 +15,17 @@ def swap(x,y):
 
 #호출
 print(swap(2,3))
+
+#디버깅 연습함수 정의
+def intersect(prelist,postlist):
+    #함수 내부의 지역 함수를 초기화(list)
+    retList = []
+    #H(0) | A(1) | M(2)
+    for x in prelist:
+        #특정 글자가 postlist에 있고 특정 글자가 아직 없다(retList)
+        if x in postlist and x not in retList:
+            retList.append(x)
+    return retList
+
+#함수를 호출
+print(intersect("HAM","SPAM"))
