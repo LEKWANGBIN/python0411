be3282f18bc4e42f2f217b583a77fa5c92c1d436
LEKWANGBIN <s25532456@naver.com>
2022-04-12 14:36:10 +0900

    람다 함수 연습

--- function3.py .py
+++ function3.py .py
@@ -29,3 +29,21 @@ def union(*ar):
 #호출
 print(union("HAM","EGG"))
 print(union("HAM","EGG","SPAM"))
+#정의되지않은 인자 처리(필수, 옵션:dict)
+def userURLBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+
+#호출
+print(userURLBuilder("credu.com", "80", id="kim", passwd="1234"))
+print(userURLBuilder("credu.com", "80", id="kim", passwd="1234", 
+    name="mike"))
+
+#람다함수(이름이 없다)
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+print((lambda x:x*x)(3))
+print(globals())
