dd28a20550cb4f7466a9e1dfdf60586d6385097e
LEKWANGBIN <s25532456@naver.com>
2022-04-12 15:47:45 +0900

    두번째 클래스 연습

--- class2.py
+++ class2.py
@@ -1,9 +1,12 @@
 # class1.py 
 #클래스정의(형식)
 class Person:
+    #클래스의 멤버변수 (주로 데이터를 공유)
+    num_person=0
     #초기화 메서드
     def __init__(self):
         self.name = "default name"
+        Person.num_person+=1
     #인스턴스 메서드 
     def print(self):
         print("My name is {0}".format(self.name))
@@ -11,6 +14,5 @@ class Person:
 #인스턴스 생성
 p1 = Person()
 p2 = Person()
-p2.name = "전우치"
-p1.print()
-p2.print()
+p3 = Person()
+print("인스턴스 개수 :", Person.num_person)
