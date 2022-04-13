d3184bafd38a6ccc22f771e9b43907879d6afadf
LEKWANGBIN <s25532456@naver.com>
2022-04-13 16:36:31 +0900

    파일 연습3

--- DemoFile.py
+++ DemoFile.py
@@ -2,7 +2,6 @@
 #문자열을 결합 연산
 url = "http://www.credu.com/?page=" + str(1) 
 print(url)
-
 #정렬방식 지정
 for x in range(1,6):
     print(x,"*",x,"=",x*x)
@@ -12,3 +11,15 @@ print("---약간 수정---")
 
 for x in range(1,6):
     print(x,"*",x,"=", str(x*x).rjust(3))
+
+print("---약간 수정---")
+for x in range(1,6):
+    print(x,"*",x,"=", str(x*x).zfill(5))
+
+#서식문자 지정
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+print("{0:,}".format(15000000))
+print("{0:e}".format(4/3))
+print("{0:f}".format(4/3))
+print("{0:.2f}".format(4/3))
