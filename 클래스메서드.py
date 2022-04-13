36dfb1f228f9efba578f75b50a53fd2e1cd77c07
LEKWANGBIN <s25532456@naver.com>
2022-04-13 09:53:46 +0900

    은행계정 클래스 연습

--- 클래스메서드.py
+++ 클래스메서드.py
@@ -1,15 +1,12 @@
 # 클래스메서드.py
+#부모클래스
 class CoeffVar(object):
     coefficient = 1 
     @classmethod
     def mul(cls, fact):
         return cls.coefficient * fact 
-
 #파생형식을 정의
 class MulFive(CoeffVar):
     coefficient = 5 
-
 x = MulFive.mul(4)
 print(x)
-
-
