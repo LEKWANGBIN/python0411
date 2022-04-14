dda3430337439ffa089b8eb2ad8f4c18bf587e52
LEKWANGBIN <s25532456@naver.com>
2022-04-14 11:16:41 +0900

    정규표현식 연습4

--- DemoRE.py
+++ DemoRE.py
@@ -3,10 +3,20 @@ import re
 
 print(dir(re))
 
-result = re.search("[0-9]*th","35th")
+result = re.search("[0-9]*th","  35th")
+#찾은 결과가 매칭 오브젝트
 print(result)
-print(reult.group())
+print(result.group())
+#약간의 함정을 추가(정확하게 일치하는 것만)
+# result = re.match("[0-9]*th", "  35th")
+# print(result)
+# print(result.group())
 
-result = re.mathch("[0-9]*th","35th")
-print(result)
-print(reult.group())
+#년도를 검색
+years = re.search("\d{4}", "올해는 2022년")
+print(years.group())
+postcode = re.search("\d{5}", "우리동네는 52000")
+print(postcode.group())
+
+print(bool(re.search("apple", "this is apple")))
+print(bool(re.match("apple", "this is apple")))
