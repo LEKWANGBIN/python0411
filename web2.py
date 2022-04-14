ea3654dc5f9158bf1216b0a3bf48845a35852a34
LEKWANGBIN <s25532456@naver.com>
2022-04-14 14:42:13 +0900

    웹크롤링 파일처리

--- web2.py
+++ web2.py
@@ -23,3 +23,21 @@ print(cartoons[0].find("a")["href"])
 for item in cartoons:
     tag = item.find("a")
     print(tag.text.strip())
+
+#파일 생성
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
+#수열함수로 페이지 번호 생성
+for i in range(1,6):
+    url = \
+    "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" \
+    + str(i)
+    print(url)
+    data = urllib.request.urlopen(url)
+    #검색이 용이한 객체 
+    soup = BeautifulSoup(data, "html.parser")
+    cartoons = soup.find_all("td", class_="title")
+    for item in cartoons:
+        tag = item.find("a")
+        print(tag.text.strip())
+        f.write(tag.text.strip() + "\n")
+f.close()
